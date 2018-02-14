52f14c53125835a684c73c341640ab33f99a3299
Dobby233Liu <papaqi1234567@126.com>
2018-02-14 21:35:55 +0800

    Git

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # would-let-you-get-the-file-path
 It ***W**ould **L**et **Y**ou **G**et **T**he **F**ile **P**ath*.
+
+Twin of [by-syk/PathCopier](https://github.com/by-syk/PathCopier).
---
+++ src/main/java/com/github/dobby233liu/android/wlygtfp/MainActivity.java
@@ -0,0 +1,109 @@
+package com.github.dobby233liu.android.wlygtfp;
+
+import android.app.*;
+import android.os.*;
+import android.content.pm.*;
+import android.*;
+import android.content.*;
+import android.net.*;
+import android.widget.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+	// Ask for permission: READ_EXTERNAL_STORAGE
+	if (Integer.valueOf(android.os.Build.VERSION.SDK) >= 23 && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+	!= PackageManager.PERMISSION_GRANTED) {
+	/*if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+	 }*/
+
+	requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+	} else{
+
+
+	Intent intent = getIntent();
+	if (intent == null){
+	return;
+	}
+	String action = intent.getAction();
+	if (Intent.ACTION_VIEW.equals(action)) {
+	Uri uri = intent.getData();
+	if (null != uri) {
+	String path=uri.getPath();
+	ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+	cm.setText(path);
+	Toast.makeText(this,"已复制位置",Toast.LENGTH_LONG).show();
+	finish();
+	}
+	/**else if(Intent.ACTION_SEND==action){
+	Bundle bundle = intent.getExtras();
+	Uri extraStream=(Uri)bundle.get(Intent.EXTRA_STREAM);
+	String extraText;
+	if(extraStream==null){
+	extraText=(String) bundle.get(Intent.EXTRA_TEXT);
+	ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+	cm.setText(extraText);
+	Toast.makeText(this,"已复制位置",Toast.LENGTH_LONG).show();
+	finish();
+	}else{
+	String path2=extraStream.getPath();
+	ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+	cm.setText(path2);
+	Toast.makeText(this,"已复制位置",Toast.LENGTH_LONG).show();
+	finish();
+	}
+	}
+	}
+    }**/}}}
+	@Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+	super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+	
+	Intent intent = getIntent();
+	if (intent == null){
+	return;
+	}
+	String action = intent.getAction();
+	if (Intent.ACTION_VIEW.equals(action)) {
+	Uri uri = intent.getData();
+	if (null != uri) {
+	String path=uri.getPath();
+	ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+	cm.setText(path);
+	Toast.makeText(this,"已复制位置",Toast.LENGTH_LONG).show();
+	finish();
+	}
+/**	else if(Intent.ACTION_SEND==action){
+	Bundle bundle = intent.getExtras();
+	Uri extraStream=(Uri)bundle.get(Intent.EXTRA_STREAM);
+	String extraText;
+	if(extraStream==null){
+	extraText=(String) bundle.get(Intent.EXTRA_TEXT);
+	ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+	cm.setText(extraText);
+	Toast.makeText(this,"已复制位置",Toast.LENGTH_LONG).show();
+	finish();
+	}else{
+		String path2=extraStream.getPath();
+	ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+	cm.setText(path2);
+	Toast.makeText(this,"已复制位置",Toast.LENGTH_LONG).show();
+	finish();
+	}}**/
+	}
+	
+	
+    }
+
+    
+	
+    
+	
+
+    
+}
